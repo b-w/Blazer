a6102db2f101cb926f08a49e52cd21beefa05e58
b-w <b.wolff@outlook.com>
2016-05-01 10:27:49 +0200

    include dynamic versions of the Blazer Linq extension methods

--- Blazer.Tests/QueryTests.cs
+++ Blazer.Tests/QueryTests.cs
@@ -63,7 +63,7 @@
         }
 
         [TestMethod]
-        public void Query_Dynamic()
+        public void Query_Dynamic_Single()
         {
             // arrange
             dynamic product;
@@ -72,8 +72,7 @@
             using (var conn = TestResources.GetAdventureWorksConnection())
             {
                 conn.Open();
-                product = conn.Query("SELECT * FROM [Production].[Product] WHERE [ProductID] = @Id", new { Id = 328 })
-                    .First();
+                product = conn.First("SELECT * FROM [Production].[Product] WHERE [ProductID] = @Id", new { Id = 328 });
                 conn.Close();
             }
 
@@ -83,6 +82,37 @@
             Assert.AreEqual("Mountain End Caps", product.Name);
             Assert.AreEqual(new DateTime(2008, 4, 30), product.SellStartDate);
             Assert.IsNull(product.SellEndDate);
+        }
+
+        [TestMethod]
+        public void Query_Dynamic_Multiple()
+        {
+            // arrange
+            IList<dynamic> products;
+
+            // act
+            using (var conn = TestResources.GetAdventureWorksConnection())
+            {
+                conn.Open();
+                products = conn.Query("SELECT * FROM [Production].[Product] WHERE [ProductID] < 100")
+                    .ToList();
+                conn.Close();
+            }
+
+            // assert
+            Assert.AreEqual(4, products.Count);
+            for (int i = 0; i < products.Count; i++)
+            {
+                Assert.IsNotNull(products[i]);
+            }
+            Assert.AreEqual(1, products[0].ProductID);
+            Assert.AreEqual("Adjustable Race", products[0].Name);
+            Assert.AreEqual(2, products[1].ProductID);
+            Assert.AreEqual("Bearing Ball", products[1].Name);
+            Assert.AreEqual(3, products[2].ProductID);
+            Assert.AreEqual("BB Ball Bearing", products[2].Name);
+            Assert.AreEqual(4, products[3].ProductID);
+            Assert.AreEqual("Headset Ball Bearings", products[3].Name);
         }
     }
 }
--- Blazer/BlazerLinqExtensions.cs
+++ Blazer/BlazerLinqExtensions.cs
@@ -10,9 +10,19 @@
             return connection.Query<T>(command, parameters, config).First();
         }
 
+        public static dynamic First(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null)
+        {
+            return connection.Query(command, parameters, config).First();
+        }
+
         public static T FirstOrDefault<T>(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null) where T : new()
         {
             return connection.Query<T>(command, parameters, config).FirstOrDefault();
+        }
+
+        public static dynamic FirstOrDefault(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null)
+        {
+            return connection.Query(command, parameters, config).FirstOrDefault();
         }
 
         public static T Single<T>(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null) where T : new()
@@ -20,9 +30,19 @@
             return connection.Query<T>(command, parameters, config).Single();
         }
 
+        public static dynamic Single(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null)
+        {
+            return connection.Query(command, parameters, config).Single();
+        }
+
         public static T SingleOrDefault<T>(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null) where T : new()
         {
             return connection.Query<T>(command, parameters, config).SingleOrDefault();
+        }
+
+        public static dynamic SingleOrDefault(this IDbConnection connection, string command, object parameters = null, CommandConfiguration config = null)
+        {
+            return connection.Query(command, parameters, config).SingleOrDefault();
         }
     }
 }
