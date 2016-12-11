4d1c0a15133cf31b900c9d1da94573d76a138c08
b-w <b.wolff@outlook.com>
2016-12-11 11:53:51 +0100

    update assembly info; add nuspec file

--- Blazer/Properties/AssemblyInfo.cs
+++ Blazer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Blazer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("High-performance ADO.NET object mapper.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bart Wolff")]
 [assembly: AssemblyProduct("Blazer")]
-[assembly: AssemblyCopyright("Copyright © Bart Wolff 2016")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Bart Wolff")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
 [assembly: AssemblyFileVersion("0.1.0.0")]
